7f0ea981b0921345805757c70b75367d506a6b5a
Matthias Osswald <mat.osswald@sap.com>
2018-04-30 10:40:47 +0200

    Adopt preload tests after UglifyJS upgrade
    Compressed files look different now.

--- test/preload/expected/component_resource_prefix/Component-preload.js
+++ test/preload/expected/component_resource_prefix/Component-preload.js
@@ -1,5 +1,5 @@
 sap.ui.require.preload({
-	"my/app/Component.js": "/* © */\n\"use strict\";function myFunction(n,o){return n+o}/**\n* This is a copyright comment\n*/\n/* (c) */\n/* released under */\n/* license */\nconsole.log(\"myJS\");",
+	"my/app/Component.js": "/* © */\n\"use strict\";\n/**\n* This is a copyright comment\n*/\n/* (c) */\n/* released under */\n/* license */function myFunction(n,o){return n+o}console.log(\"myJS\");",
 	"my/app/view/my.view.xml": "<my>XML</my>\n",
 	"my/app/view/myHtmlPre.view.xml": "<my>XML</my>\n<!-- A comment in XML is the same as in HTML -->\n<html:pre>   </html:pre>"
 }, "my/app/Component-preload");
